b15f4529dcb958a776728eea86986572898e47e0
Amardeep Yadav <Amardeepyadav5156@gmail.com>
2024-10-18 20:44:16 +0530

    Added the Backend with Login and Register functionality that will be accessible for the particular defined api

---
+++ backend/backendapp/serializers.py
@@ -0,0 +1,20 @@
+from rest_framework import serializers
+from django.contrib.auth import get_user_model
+
+User  = get_user_model()
+
+class UserRegistrationSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = User
+        fields = ['username', 'email', 'password']
+        extra_kwargs = {'password': {'write_only': True}}
+
+    def create(self, validated_data):
+        user = User(**validated_data)
+        user.set_password(validated_data['password'])  # Encrypt password
+        user.save()
+        return user
+
+class UserLoginSerializer(serializers.Serializer):
+    email = serializers.EmailField()
+    password = serializers.CharField()
